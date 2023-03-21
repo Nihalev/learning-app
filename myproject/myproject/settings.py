a6db09dd6629a3531acce71e0df24c72fff2cd9d
Nihalev <nihalev111@email.com>
2023-03-21 06:45:18 +0530

    nihalev

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-h3i_xwx$9_qaw7^*t^-ub^ln$nw37(&-%5bk-24%tg+fyx(!=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
